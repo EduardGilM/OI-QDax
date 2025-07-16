8f1e6935afab36b89c5c9fd43266b65a02199971
EduardGil <eduardgilmagraner@gmail.com>
2025-07-16 10:19:33 +0200

    Debug

--- qdax/environments/wrappers.py
+++ qdax/environments/wrappers.py
@@ -272,10 +272,10 @@ class LZ76Wrapper(Wrapper):
             normalized_complexity = jnp.clip((raw_complexity - lz76_min) / (lz76_max - lz76_min), 0.0, 1.0)
             normalized_o_info = jnp.clip(2.0 * ((raw_o_info - oi_min) / (oi_max - oi_min)) - 1.0, -1.0, 1.0)
 
-            jax.debug.print("Raw complexity: {x}", x=raw_complexity)
-            jax.debug.print("Raw o-info: {x}", x=raw_o_info)
-            jax.debug.print("Normalized complexity: {x}", x=normalized_complexity)
-            jax.debug.print("Normalized o-info: {x}", x=normalized_o_info)
+            #jax.debug.print("Raw complexity: {x}", x=raw_complexity)
+            #jax.debug.print("Raw o-info: {x}", x=raw_o_info)
+            #jax.debug.print("Normalized complexity: {x}", x=normalized_complexity)
+            #jax.debug.print("Normalized o-info: {x}", x=normalized_o_info)
             
             return raw_complexity, raw_o_info, jnp.array([normalized_complexity, normalized_o_info])
         
@@ -290,10 +290,10 @@ class LZ76Wrapper(Wrapper):
             obs_sequence
         )
 
-        #jax.debug.print("Current step: {x}", x=current_step)
-        #jax.debug.print("Complexity: {x}", x=complexities)
-        #jax.debug.print("O-Information: {x}", x=o_info_values)
-        #jax.debug.print("State descriptor: {x}", x=state_descriptor)
+        jax.debug.print("Current step: {x}", x=current_step)
+        jax.debug.print("Complexity: {x}", x=complexities)
+        jax.debug.print("O-Information: {x}", x=o_info_values)
+        jax.debug.print("State descriptor: {x}", x=state_descriptor)
 
         state.info.update({
             "obs_sequence": obs_sequence,
